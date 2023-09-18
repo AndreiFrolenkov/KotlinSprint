d1475a5efe17b0024a39b7fc2b038a1a375b67fd
Andrei Frolenkov <frolenkov87@gmail.com>
2023-09-18 23:19:27 +0300

    KS-2-3 решена и отправлена на проверку.

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,25 @@
+package lesson_2
+
+/*Задача 3 к Уроку 2
+
+Сайт с расписанием поездов получает данные с сервера.
+Сервер посылает время выезда и время в пути, а время прибытия вычисляется из них.
+Сервер прислал данные, что поезд выехал в 9:39 и будет в пути 457 минут.
+ – Создай целочисленные переменные и проинициализируй их этими данными;
+ – Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
+ – Выведи результат в консоль.
+*/
+
+fun main() {
+
+    val departHour = 9
+    val departMinute = 39
+    val travelTime = 457
+    val departureTimeInMinutes = departHour * 60 + departMinute
+    val totalMinutes = departureTimeInMinutes + travelTime
+    val arrivalHour = totalMinutes / 60
+    val arrivalMinute = totalMinutes % 60 + departMinute
+
+    val result = String.format("%s:%s", arrivalHour, arrivalMinute)
+    println(result)
+}
